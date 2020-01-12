80c9896ed78f08ad74474b07a1fa54b20e13aaff
Jovan100 <jovan.stankovic993@gmail.com>
2020-01-12 14:35:27 +0100

    Izbačen model Group iz admin UI

--- korisnik/admin.py
+++ korisnik/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from korisnik.models import Korisnik
+from django.contrib.auth.models import Group
 
 class KorisnikAdmin(admin.ModelAdmin):
     search_fields = ['ime', 'prezime', 'email']
@@ -8,3 +9,4 @@ class KorisnikAdmin(admin.ModelAdmin):
         model = Korisnik
 
 admin.site.register(Korisnik, KorisnikAdmin)
+admin.site.unregister(Group)
