3357ed8f770123facdf4b83af45fa6722c7c614e
Jovan100 <jovan.stankovic993@gmail.com>
2020-01-12 21:27:25 +0100

    Kreiran TemplateView za početnu stranu

--- destinacija/views.py
+++ destinacija/views.py
@@ -1,3 +1,13 @@
+from django.views.generic import DetailView, ListView
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from destinacija.models import (
+    Agencija,
+    Cenovnik,
+    Destinacija,
+    KategorijaDestinacije,
+    Zemlja
+)
 
-# Create your views here.
+class PocetnaStranaView(TemplateView):
+    template_name = 'destinacija/pocetna_strana.html'
