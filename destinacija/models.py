02893fad98f4d6a8c5e3ca6a224645236a6cce2d
Jovan100 <jovan.stankovic993@gmail.com>
2020-01-12 14:42:40 +0100

    Kreiran model KategorijaDestinacije

--- destinacija/models.py
+++ destinacija/models.py
@@ -9,3 +9,13 @@ class Zemlja(models.Model):
 
     class Meta:
         verbose_name_plural = "Zemlje"
+
+class KategorijaDestinacije(models.Model):
+    naziv = models.CharField(max_length=100, unique=True)
+    slika = models.ImageField(upload_to='slike', blank=True, null=True)
+
+    def __str__(self):
+        return self.naziv
+
+    class Meta:
+        verbose_name_plural = "Kategorije Destinacija"
