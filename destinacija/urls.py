12821c386e80d1ae510cc2947bbf1128eed92cdf
Jovan100 <jovan.stankovic993@gmail.com>
2020-01-14 15:40:23 +0100

    Kreirana ruta do stranice tirističkih pojmova

--- destinacija/urls.py
+++ destinacija/urls.py
@@ -3,5 +3,6 @@ from destinacija.views import *
 
 urlpatterns = [
     path('', PocetnaStranaView.as_view(), name='pocetna_strana'),
-    path('Opšti_Uslovi_Putovanja/', UsloviPutovanjaView.as_view(), name='opsti_uslovi_putovanja')
+    path('Opšti_Uslovi_Putovanja/', UsloviPutovanjaView.as_view(), name='opsti_uslovi_putovanja'),
+    path('Turistički_Pojmovi/', TuristickiPojmoviView.as_view(), name='turisticki_pojmovi'),
 ]
