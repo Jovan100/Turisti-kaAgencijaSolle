445c5b5b8f8c8da834259ca6b9109766f6051038
Jovan100 <jovan.stankovic993@gmail.com>
2020-01-19 11:16:37 +0100

    Kreiran View za stranicu Grcka

--- destinacija/views.py
+++ destinacija/views.py
@@ -1,12 +1,18 @@
 from django.views.generic import DetailView, ListView
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from django.views import View
 from destinacija.models import (
     Agencija,
     Cenovnik,
     Destinacija,
+    Regija,
     KategorijaDestinacije,
     Zemlja
+)
+from django.views.generic import (
+    DetailView,
+    ListView,
 )
 
 class PocetnaStranaView(TemplateView):
@@ -29,3 +35,37 @@ class ONamaView(TemplateView):
 
 class KontaktView(TemplateView):
     template_name = 'destinacija/kontakt.html'
+
+class GrckaListView(View):
+    def get(self, request, *args, **kwargs):
+        zemlja = Zemlja.objects.get(naziv="Grčka")
+        kopno = KategorijaDestinacije.objects.get(naziv="Kopno")
+        ostrvo = KategorijaDestinacije.objects.get(naziv="Ostrva")
+        orfanska = Regija.objects.get(naziv="Orfanski zaliv")
+        egej = Regija.objects.get(naziv="Egejska regija")
+        jonska = Regija.objects.get(naziv="Jonska regija")
+        olimpska = Regija.objects.get(naziv="Olimpska regija")
+        atos = Regija.objects.get(naziv="Atos - treći prst")
+        sitonija = Regija.objects.get(naziv="Sitonija - drugi prst")
+        kasandra = Regija.objects.get(naziv="Kasandra - prvi prst")
+        kopno_orfanska = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=orfanska)
+        kopno_egej = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=egej)
+        kopno_jonska = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=jonska)
+        kopno_olimpska = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=olimpska)
+        kopno_atos = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=atos)
+        kopno_sitonija = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=sitonija)
+        kopno_kasandra = Destinacija.objects.filter(zemlja=zemlja, kategorija=kopno, regija=kasandra)
+        ostrva_egej = Destinacija.objects.filter(zemlja=zemlja, kategorija=ostrvo, regija=egej)
+        ostrva_jonska = Destinacija.objects.filter(zemlja=zemlja, kategorija=ostrvo, regija=jonska)
+        return render(request, 'destinacija/grcka.html',
+            {
+            'kopno_orfanska': kopno_orfanska,
+            'kopno_egej': kopno_egej,
+            'kopno_jonska': kopno_jonska,
+            'kopno_olimpska': kopno_olimpska,
+            'kopno_atos': kopno_atos,
+            'kopno_sitonija': kopno_sitonija,
+            'kopno_kasandra': kopno_kasandra,
+            'ostrva_egej': ostrva_egej,
+            'ostrva_jonska': ostrva_jonska,
+            })
